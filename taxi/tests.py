5cff393d3ea4f71e560b9f2180ad0575584b307b
Nikita Tkachenko <7heswatgg@gmail.com>
2025-04-22 05:49:00 +0200

    add search to views and some tests for them

--- taxi/tests.py
+++ taxi/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.urls import reverse
+from taxi.models import Manufacturer, Driver, Car
 
-# Create your tests here.
+
+class SearchTests(TestCase):
+    def setUp(self):
+        self.driver = Driver.objects.create_user(
+            username="john", password="password", license_number="ABC12345"
+        )
+        self.driver2 = Driver.objects.create_user(
+            username="doe", password="password", license_number="XYZ54321"
+        )
+        self.manufacturer = Manufacturer.objects.create(name="Tesla", country="USA")
+        self.car = Car.objects.create(model="Model S", manufacturer=self.manufacturer)
+
+    def test_search_driver_by_username(self):
+        response = self.client.login(username="john", password="password")
+        response = self.client.get(reverse("taxi:driver-list") + "?username=john")
+        self.assertContains(response, "john")
+        self.assertNotContains(response, "doe")
+
+    def test_search_car_by_model(self):
+        self.client.login(username="john", password="password")
+        response = self.client.get(reverse("taxi:car-list") + "?model=Model S")
+        self.assertContains(response, "Model S")
+
+    def test_search_manufacturer_by_name(self):
+        self.client.login(username="john", password="password")
+        response = self.client.get(reverse("taxi:manufacturer-list") + "?name=Tesla")
+        self.assertContains(response, "Tesla")
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_bootstrap4",
     "debug_toolbar",
     "crispy_forms",
     "taxi",
@@ -143,3 +144,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
